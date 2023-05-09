82f37849a91aef8354de6bac4a3a0bdd20f5d553
Vadim Yevstafiev <eustas1976@gmail.com>
2023-05-09 20:01:07 +0300

    Homework 4

--- index.php
+++ index.php
@@ -1,16 +1,70 @@
 <?php
 
-const DSN = "pgsql:host=db;dbname=test";
+header('Content-Type: text/plain; charset=UTF-8');
 
-const USER = "postgres";
+try {
 
-const PASS = "example";
+    $user = new User('Ivan', 35, 'user@mail.com');
 
-$pdo = new PDO(DSN, USER, PASS, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+    var_dump($user->getAll());
 
-$query = $pdo->prepare("SELECT * FROM students");
+    $user->setName('Peter');
 
-$query->execute();
+    var_dump($user->getAll());
 
-echo '<pre>' . print_r($query->fetchAll(), true) . '</pre>';
+    $user->setAge(45);
+
+    var_dump($user->getAll());
+
+    $user->setEmail('post@mail.com');
+
+} catch (badMethodNameException $e) {
+
+    echo 'Выброшено пользовательское исключение badMethodNameException: ' . $e->getMessage() . PHP_EOL;
+
+} catch (Exception $e) {
+
+    echo 'Выброшено исключение: ' . $e->getMessage() . PHP_EOL;
+
+}
+
+class User {
+
+    public function __construct(private string $name, private int $age, private string $email) {}
+
+    public function __call(string $methodName, array $arguments) {
+
+        if (!(method_exists($this, $methodName))) {
+
+            throw new badMethodNameException($methodName);
+
+        }
+
+        call_user_func_array(array($this, $methodName), $arguments);
+    }
+
+    public function getAll() {
+        return array(
+            'name' => $this->name,
+            'age' => $this->age,
+            'email' => $this->email
+        );
+    }
+
+    private function setName(string $name) {
+        $this->name = $name;
+    }
+
+    private function setAge(int $age) {
+        $this->age = $age;
+    }
+}
+
+class badMethodNameException extends Exception {
+
+    public function __construct(string $methodName) {
+        $message = 'Попытка вызвать несуществующий метод ' . $methodName;
+        parent::__construct($message);
+    }
+ }
 ?>
