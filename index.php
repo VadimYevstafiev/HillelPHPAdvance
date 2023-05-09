de2f5b30361df292d4e03a88b4cd691b7247026a
Vadim Yevstafiev <eustas1976@gmail.com>
2023-05-09 13:50:29 +0300

    Homework 2

--- index.php
+++ index.php
@@ -1,16 +1,108 @@
 <?php
 
-const DSN = "pgsql:host=db;dbname=test";
+header('Content-Type: text/plain; charset=UTF-8');
 
-const USER = "postgres";
+$color = new ValueObject(30,30,30);
 
-const PASS = "example";
+var_dump($color->equals(new ValueObject(30,30,30)));
 
-$pdo = new PDO(DSN, USER, PASS, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+var_dump($color->equals(new ValueObject(30,35,30)));
 
-$query = $pdo->prepare("SELECT * FROM students");
+$color = ValueObject::random();
 
-$query->execute();
+var_dump($color);
 
-echo '<pre>' . print_r($query->fetchAll(), true) . '</pre>';
+$color = new ValueObject(10,20,30);
+
+var_dump($color->mix(new ValueObject(20,30,40)));
+
+$color = new ValueObject(0,300,0);
+
+class ValueObject {
+
+    private int $red;
+
+    private int $green;
+
+    private int $blue;
+
+    public function __construct(int $red, int $green, int $blue) 
+    {
+        $this::setRed($red);
+        $this::setGreen($green);
+        $this::setBlue($blue);
+    }
+
+    public function getRed(): int 
+    {
+        return $this->red;
+    }
+
+    public function getGreen(): int 
+    {
+        return $this->green;
+    }
+
+    public function getBlue(): int 
+    {
+        return $this->blue;
+    }
+
+    public function equals(ValueObject $object): bool 
+    {
+        $functions = array('getRed', 'getGreen', 'getBlue');
+        foreach ($functions as $value) 
+        {
+            if ($this->$value() != $object->$value()) 
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static public function random(): object 
+    {
+        return new ValueObject(rand(0, 255), rand(0, 255), rand(0, 255));
+    }
+
+    public function mix(ValueObject $object): object 
+    {
+        $red = (int) (0.5 * ($this->getRed() + $object->getRed()));
+        $green = (int) (0.5 * ($this->getGreen() + $object->getGreen()));
+        $blue = (int) (0.5 * ($this->getBlue() + $object->getBlue()));
+        return new ValueObject($red, $green, $blue);
+    }
+
+    private function setRed(int $value) 
+    {
+        $this::setColorValue('red', $value);
+    }
+
+    private function setGreen(int $value) 
+    {
+        $this::setColorValue('green', $value);
+    }
+
+    private function setBlue(int $value) 
+    {
+        $this::setColorValue('blue', $value);
+    }
+
+    private function setColorValue(string $name, int $value) 
+    {
+        $this->$name = $this::checkColorValue($value) ? $value : $this::badColorValue($name);
+    }
+
+    private function checkColorValue(int $value): bool
+    {
+        return ($value >=0 && $value <=255) ? true : false;
+    }
+
+    private function badColorValue(string $name)
+    {
+        throw new Exception ('Введено некоректное значение свойства ' . $name . 
+        '. Свойство ' . $name . ' должно иметь значение в диапзоне от 0 до 255');
+    }
+}
 ?>
