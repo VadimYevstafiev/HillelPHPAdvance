58b75b1515566ecc67889e6186a1d5cc0ed608ca
Vadim Yevstafiev <eustas1976@gmail.com>
2023-05-12 19:08:59 +0300

    Homework 5

--- index.php
+++ index.php
@@ -1,16 +1,66 @@
 <?php
 
-const DSN = "pgsql:host=db;dbname=test";
+header('Content-Type: text/plain; charset=UTF-8');
 
-const USER = "postgres";
+$users = [
+    array('id' => 245, 'password' => 'secret'),
+    array('id' => 'user', 'password' => 'example'),
+    array('id' => 423, 'password' => 'secret1234')
+];
 
-const PASS = "example";
+foreach ($users as $value) {
 
-$pdo = new PDO(DSN, USER, PASS, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+    testUser ($value['id'], $value['password']);
 
-$query = $pdo->prepare("SELECT * FROM students");
+}
 
-$query->execute();
 
-echo '<pre>' . print_r($query->fetchAll(), true) . '</pre>';
+
+function testUser (mixed $id, string $password) {
+
+    try {
+
+        $user = new User($id, $password);
+    
+        var_dump($user->getUserData());
+    
+    } catch (Exception $e) {
+    
+        echo 'Выброшено исключение: "' . $e->getMessage() 
+        . '" В файле: ' . $e->getFile() . ', строке: ' . $e->getLine() . PHP_EOL;
+    
+    }
+
+}
+
+class User {
+
+    public function __construct(private mixed $id, private string $password) {
+        $this::setID($id);
+        $this::setPassword($password);
+    }
+
+    public function getUserData() {
+        return array(
+            'id' => $this->id,
+            '$password' => $this->password
+        );
+    }
+
+    private function setID(mixed $id) {
+        if (is_int($id)) {
+            $this->id = $id;
+        } else {
+            throw new Exception ('Введено некорректное значение поля id. Поле id должно быть числом.');
+        }
+    }
+
+    private function setPassword(string $password) {
+        if (strlen($password) <= 8) {
+            $this->password = $password;
+        } else {
+            throw new Exception ('Введено некорректное значение поля password. Поле password должно иметь длину не более 8 символов.');
+        }
+    }
+}
 ?>
